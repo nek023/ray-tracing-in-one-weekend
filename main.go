66d635f76fcc407936910b07372070b294650956
questbeat <questbeat@gmail.com>
2019-12-30 01:18:11 +0900

    golang/geoのr3.Vectorを使う

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/golang/geo/r3"
+)
 
 func main() {
 	nx := 200
@@ -8,12 +12,14 @@ func main() {
 	fmt.Printf("P3\n%d %d\n255\n", nx, ny)
 	for j := ny - 1; j >= 0; j-- {
 		for i := 0; i < nx; i++ {
-			r := float64(i) / float64(nx)
-			g := float64(j) / float64(ny)
-			b := 0.2
-			ir := int(255.990 * r)
-			ig := int(255.990 * g)
-			ib := int(255.990 * b)
+			col := r3.Vector{
+				float64(i) / float64(nx),
+				float64(j) / float64(ny),
+				0.2,
+			}
+			ir := int(255.990 * col.X)
+			ig := int(255.990 * col.Y)
+			ib := int(255.990 * col.Z)
 			fmt.Printf("%d %d %d\n", ir, ig, ib)
 		}
 	}
